ea504ea5d610c6727b6fd52cafdf30a13c41cdf2
Armaggheddon <alebrum97@gmail.com>
2024-12-15 16:40:31 +0100

    Initial commit

---
+++ bricks_finder/src/model_helper.py
@@ -0,0 +1,6 @@
+HF_MODEL_MINIFIGURES_REPO_ID = "armaggheddon97/clip-vit-base-patch32_lego-minifigures"
+HF_MODEL_BRICKS_REPO_ID = "armaggheddon97/clip-vit-base-patch32_lego-bricks"
+
+class ModelHelper:
+    def __init__(self, index_root):
+        pass
---
+++ bricks_finder/src/query_helper.py
@@ -0,0 +1,6 @@
+HF_MINIFIGURES_REPO_ID = "armaggheddon97/lego_minifigure_captions"
+HF_BRICKS_REPO_ID = "armaggheddon97/lego_bricks_captions"
+
+class QueryHelper:
+    def __init__(self, index_root):
+        pass
