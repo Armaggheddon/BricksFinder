02d2a56a15293bf715fc79c3f3f868eaa4666483
Armaggheddon <alebrum97@gmail.com>
2024-12-17 23:22:46 +0100

    Merged into QueryHelper

--- bricks_finder/src/model_helper.py
+++
@@ -1,6 +0,0 @@
-HF_MODEL_MINIFIGURES_REPO_ID = "armaggheddon97/clip-vit-base-patch32_lego-minifigures"
-HF_MODEL_BRICKS_REPO_ID = "armaggheddon97/clip-vit-base-patch32_lego-bricks"
-
-class ModelHelper:
-    def __init__(self, index_root):
-        pass
