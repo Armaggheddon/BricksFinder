9a690a3697c25d86f77ac21f396fd2641971a824
Armaggheddon <alebrum97@gmail.com>
2024-12-17 23:22:33 +0100

    Implemented query logic for minifigure model/dataset/vector_index

--- bricks_finder/src/main.py
+++ bricks_finder/src/main.py
@@ -1,87 +1,26 @@
-import time
-import random
+from pathlib import Path
 
 import gradio as gr
-import numpy as np
-from PIL import Image
+
+from query_helper import QueryHelper, QueryResult, IndexType
 
 
+THIS_PATH = Path(__file__).parent.parent
+VECTOR_INDEX_ROOT = THIS_PATH / "vector_indexes"
 
-dummy_images = {
-    "minifigures" : [
-        (Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)), "0"),
-        (Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)), "1"),
-        (Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)), "2"),
-        (Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)), "3"),
-        (Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)), "4"),
-        (Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)), "5"),
-        (Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)), "6"),
-        (Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)), "7"),
-        (Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)), "8"),
-        (Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)), "9"),
-    ],
-    "bricks" : [
-        (Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)), "0"),
-        (Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)), "1"),
-        (Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)), "2"),
-        (Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)), "3"),
-        (Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)), "4"),
-        (Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)), "5"),
-        (Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)), "6"),
-        (Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)), "7"),
-        (Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)), "8"),
-        (Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)), "9"),
-    ]
-}
 
-dummy_additional_info = {
-    "minifigures" : [
-        "Minifigure 1",
-        "Minifigure 2",
-        "Minifigure 3",
-        "Minifigure 4",
-        "Minifigure 5",
-        "Minifigure 6",
-        "Minifigure 7",
-        "Minifigure 8",
-        "Minifigure 9",
-        "Minifigure 10",
-    ],
-    "bricks" : [
-        "Brick 1",
-        "Brick 2",
-        "Brick 3",
-        "Brick 4",
-        "Brick 5",
-        "Brick 6",
-        "Brick 7",
-        "Brick 8",
-        "Brick 9",
-        "Brick 10",
-    ]
-}
+query_helper = QueryHelper(VECTOR_INDEX_ROOT)
+
 
 def run_query(query, images, dataset):
-    # Simulate a long running query
-
-    time.sleep(1)
-    if query == "":
-        return None, None
-    
-    if dataset == "minifigures":
-        imgs = dummy_images["minifigures"]
-    else:
-        imgs = dummy_images["bricks"]
-    
-    random.shuffle(imgs)
-    imgs_sub = imgs[:5] # simulate limited results
-    return imgs_sub, None # no selected image yet
+    results: list[QueryResult] = query_helper.query(query, 3)
+    return [(result.image, result.idx) for result in results], None # no selected image yet
 
 def select_image(evt: gr.SelectData):
     select_image = evt.value
     image_id = int(select_image["caption"])
-    dataset_id = dataset_radio.value
-    return dummy_additional_info[dataset_id][image_id], dummy_images[dataset_id][image_id][0]
+    additional_info, image = query_helper.get_image_info(image_id)
+    return additional_info, image
 
 
 with gr.Blocks() as demo:
@@ -95,6 +34,7 @@ with gr.Blocks() as demo:
         gallery = gr.Gallery(
             label="Search results",
             columns=3,
+            rows=3
         )
 
     with gr.Row():
--- bricks_finder/src/query_helper.py
+++ bricks_finder/src/query_helper.py
@@ -1,8 +1,14 @@
 from pathlib import Path
 from enum import Enum
+from io import StringIO
+from dataclasses import dataclass
 
 import faiss
+import tqdm
+from PIL import Image
+from PIL.Image import Image as PILImage
 import torch
+from torchvision.transforms.functional import to_tensor, resize
 from loguru import logger
 from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, Normalize, InterpolationMode
 from datasets import load_dataset
@@ -14,10 +20,25 @@ HF_MINIFIGURE_MODEL_ID = "armaggheddon97/clip-vit-base-patch32_lego-minifigure"
 HF_BRICKS_REPO_ID = "armaggheddon97/lego_bricks_captions"
 
 
-
 class IndexType(Enum):
     MINIFIGURE = "minifigure"
     BRICK = "brick"
+
+    @staticmethod
+    def from_str(value: str) -> 'IndexType':
+        if value == "minifigure":
+            return IndexType.MINIFIGURE
+        elif value == "brick":
+            return IndexType.BRICK
+        else:
+            raise ValueError(f"Invalid IndexType: {value}")
+
+@dataclass
+class QueryResult:
+    idx: int
+    image: PILImage
+    additional_info: str
+    distance: float
 
 
 class QueryHelper:
@@ -26,31 +47,41 @@ class QueryHelper:
         self.minifigure_root = vector_index_root / "minifigures"
         self.minifigure_dataset = self.minifigure_root / "dataset_cache"
         self.minifigure_model = self.minifigure_root / "model_cache"
+        self.minifigure_tokenizer = self.minifigure_root / "tokenizer_cache"
         self.minifigure_index = self.minifigure_root / "index.faiss"
         self.brick_root = vector_index_root / "bricks"
         self.brick_dataset = self.brick_root / "dataset_cache"
         self.brick_model = self.brick_root / "model_cache"
+        self.brick_tokenizer = self.brick_root / "tokenizer_cache"
         self.brick_index = self.brick_root / "index.faiss"
 
         self.minifigure_root.mkdir(parents=True, exist_ok=True)
         self.brick_root.mkdir(parents=True, exist_ok=True)
 
-        self.image_transform = Compose([
-            Resize(224, interpolation=InterpolationMode.BICUBIC),
-            CenterCrop(224),
-            ToTensor(),
-            Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
-        ])
-        self.image_transform = torch.jit.script(self.image_transform)
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model_dtype = torch.float16 if self.device == "cuda" else torch.float32
 
-        load_dataset(
-            HF_MINIFIGURES_DATASET_ID,
-            cache_dir=self.minifigure_root,
-            split="train",
-            download_mode="reuse_cache_if_exists",
-        )
+        class ImageTransform(torch.nn.Module):
+            def __init__(self):
+                super(ImageTransform, self).__init__()
+                self.transform = Compose([
+                    Resize(224, interpolation=InterpolationMode.BICUBIC),
+                    CenterCrop(224),
+                    Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
+                ])
+            def forward(self, x) -> torch.Tensor:
+                return self.transform(x)
+
+        image_transform = ImageTransform()
+        self.image_transform = torch.compile(image_transform).to(self.device)
+
+        self.current_index_type = default_index
+        self.model, self.tokenizer = self._load_model()
+        self.dataset = self._load_dataset()
+
         if not self.minifigure_index.exists():
-            pass
+            self._build_index(IndexType.MINIFIGURE, self.minifigure_index)
+        self.vector_index = self._load_vector_index()
         
         # load_dataset(
         #     HF_BRICKS_REPO_ID,
@@ -61,77 +92,155 @@ class QueryHelper:
         # if not self.brick_index.exists():
         #     self.build_index(IndexType.BRICK)
 
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.model_dtype = torch.float16 if self.device == "cuda" else torch.float32
+    def _free_resources(self):
+        logger.info(f"Releasing resources for {self.current_index_type} dataset and model")
+        if self.dataset is not None:
+            del self.dataset
+        if self.model is not None:
+            del self.model
+            if self.device == "cuda":
+                torch.cuda.empty_cache()
+        if self.tokenizer is not None:
+            del self.tokenizer
+        if self.vector_index is not None:
+            del self.vector_index
+        logger.info(f"Resources released for {self.current_index_type} dataset and model")
+    
+    def _load_model(self):
+        model_id = (
+            HF_MINIFIGURE_MODEL_ID 
+            if self.current_index_type == IndexType.MINIFIGURE 
+            else HF_BRICKS_REPO_ID
+        )
+        model_cache = (
+            self.minifigure_model 
+            if self.current_index_type == IndexType.MINIFIGURE 
+            else self.brick_model
+        )
+        tokenizer_cache = (
+            self.minifigure_tokenizer 
+            if self.current_index_type == IndexType.MINIFIGURE 
+            else self.brick_tokenizer
+        )
+        model = CLIPModel.from_pretrained(
+            model_id, 
+            cache_dir=model_cache, 
+            torch_dtype=self.model_dtype
+        ).to(self.device)
+        logger.info(f"Loaded {model_id} model")
 
-        self.load_model(default_index)
+        tokenizer = CLIPTokenizerFast.from_pretrained(
+            model_id,
+            cache_dir=tokenizer_cache
+        )
+        logger.info(f"Loaded {model_id} tokenizer")
+        return model, tokenizer
 
-        
-    def load_model(self, index_type: IndexType):
-        # free up resources
-        del self.dataset
-        del self.model
-        if self.device == "cuda":
-            torch.cuda.empty_cache()
-        del self.vector_index
-
-        dataset_id = HF_MINIFIGURES_DATASET_ID if index_type == IndexType.MINIFIGURE else HF_BRICKS_REPO_ID
-        model_id = HF_MINIFIGURE_MODEL_ID if index_type == IndexType.MINIFIGURE else HF_BRICKS_REPO_ID
-        model_cache = self.minifigure_model if index_type == IndexType.MINIFIGURE else self.brick_model
-        index_path = self.minifigure_index if index_type == IndexType.MINIFIGURE else self.brick_index
-        dataset_cache = self.minifigure_dataset if index_type == IndexType.MINIFIGURE else self.brick_dataset
-        
-        self.dataset = load_dataset(
+    
+    def _load_dataset(self):
+        dataset_id = (
+            HF_MINIFIGURES_DATASET_ID 
+            if self.current_index_type == IndexType.MINIFIGURE 
+            else HF_BRICKS_REPO_ID
+        )
+        dataset_cache = (
+            self.minifigure_dataset 
+            if self.current_index_type == IndexType.MINIFIGURE 
+            else self.brick_dataset
+        )
+        dataset = load_dataset(
             dataset_id,
             cache_dir=dataset_cache,
             split="train",
             download_mode="reuse_cache_if_exists",
         )
+        logger.info(f"Loaded {self.current_index_type.value} dataset")        
+        return dataset
 
-        self.model = CLIPModel.from_pretrained(
-            model_id, 
-            cache_dir=model_cache, 
-            device=self.device, 
-            dtype=self.model_dtype
+    def _load_vector_index(self):
+        index_path = (
+            self.minifigure_index 
+            if self.current_index_type == IndexType.MINIFIGURE 
+            else self.brick_index
         )
+        vector_index = faiss.read_index(str(index_path))
+        logger.info(f"Loaded {self.current_index_type.value} vector index")
+        return vector_index
+        
+    def _switch_dataset(self, index_type: IndexType = IndexType.MINIFIGURE):
+        if self.current_index_type == index_type:
+            logger.info(f"Already using {index_type} dataset and model")
+            return
 
-        self.vector_index = faiss.read_index(str(index_path))
-        self.loaded_index = index_type
+        self._free_resources()
 
-    def build_index(
+        self.current_index_type = index_type
+
+        self.model, self.tokenizer = self._load_model()
+        self.dataset = self._load_dataset()
+        self.vector_index = self._load_vector_index()
+
+
+    def _build_index(
         self, 
-        dataset_path: Path, 
-        model_id: str, 
-        model_cache: Path,
+        index_type: IndexType,
         index_path: Path
     ):
-        dataset = load_dataset(
-            dataset_path,
-            split="train",
-        )
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        model_dtype = torch.float16 if device == "cuda" else torch.float32
-        model = CLIPModel.from_pretrained(
-            model_id, 
-            cache_dir=model_cache, 
-            device=device, 
-            dtype=model_dtype
-        )
+        self._switch_dataset(index_type)
 
         # generate embedding for each image in the dataset
-        vector_index = faiss.IndexFlatIP(768)
-        for i, example in enumerate(dataset):
+        vector_index = faiss.IndexFlatIP(512)
+        for i, example in tqdm.tqdm(enumerate(self.dataset)):
             image = example["image"]
-            patches = self.image_transform(image).unsqueeze(0)
-            image_features = model.get_image_features(**patches).cpu().numpy()
+            image_tensor = to_tensor(image).to(self.device)
+            patches = self.image_transform(image_tensor).unsqueeze(0)
+            image_features = self.model.get_image_features(patches).detach().cpu().numpy()
             vector_index.add(image_features)
         
         # save the index
         faiss.write_index(vector_index, str(index_path))
 
-        # this is a one time operation, clear resources at end
-        del dataset
-        del model
-        if device == "cuda":
-            torch.cuda.empty_cache()
-        del vector_index
+    def query(self, query: str | PILImage, top_k: int, index_type: IndexType = None) -> list[QueryResult]:
+        embeddings = None
+        if isinstance(query, str):
+            # query is a text to be embedded
+            input_tokens = self.tokenizer(
+                query,
+                return_tensors="pt",
+                padding="max_length",
+                max_length=77,
+                truncation=True
+            ).to(self.device)
+            embeddings = self.model.get_text_features(**input_tokens).detach().cpu().numpy()
+        else:
+            input_tokens = self.image_transform(query).unsqueeze(0).to(self.device)
+            embeddings = self.model.get_image_features(**input_tokens).detach().cpu().numpy()
+
+        D, I = self.vector_index.search(embeddings, top_k)
+        results = []
+
+        for i, idx in enumerate(I[0]):
+            image = self.dataset[int(idx)]["image"]
+            
+            additional_info = StringIO()
+            for key, value in self.dataset[int(idx)].items():
+                if key != "image":
+                    additional_info.write(f"{key}: {value}\n")
+            
+            results.append(
+                QueryResult(
+                    idx=str(idx),
+                    image=image,
+                    additional_info=additional_info.getvalue(),
+                    distance=D[0][i]
+                )
+            )
+
+        return results
+    
+    def get_image_info(self, idx: int, index_type: IndexType = None) -> tuple[str, PILImage]:
+        additional_info = StringIO()
+        for key, value in self.dataset[idx].items():
+            if key != "image":
+                additional_info.write(f"{key}: {value}\n")
+        return additional_info.getvalue(), self.dataset[idx]["image"]
