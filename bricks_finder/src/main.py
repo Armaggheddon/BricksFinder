3e28b6a7bdf7bd648207dcd244a9fc004707aa34
Armaggheddon <alebrum97@gmail.com>
2024-12-18 21:19:55 +0100

    Updated ui

--- bricks_finder/src/main.py
+++ bricks_finder/src/main.py
@@ -1,58 +1,106 @@
 from pathlib import Path
+import argparse
 
 import gradio as gr
 
-from query_helper import QueryHelper, QueryResult, IndexType
+from query_helper import QueryHelper, IndexType
 
 
 THIS_PATH = Path(__file__).parent.parent
 VECTOR_INDEX_ROOT = THIS_PATH / "vector_indexes"
-
-
 query_helper = QueryHelper(VECTOR_INDEX_ROOT)
 
+def search(text_query, image_query, index_type, result_count):
+    results = query_helper.query(
+        query=text_query if image_query is None else image_query,
+        top_k=result_count,
+        index_type=IndexType.from_str(index_type)
+    )
 
-def run_query(query, images, dataset):
-    results: list[QueryResult] = query_helper.query(query, 3)
-    return [(result.image, result.idx) for result in results], None # no selected image yet
+    return [(result.image, str(result.idx)) for result in results]
 
-def select_image(evt: gr.SelectData):
+def get_image_info(evt: gr.SelectData):
     select_image = evt.value
     image_id = int(select_image["caption"])
     additional_info, image = query_helper.get_image_info(image_id)
     return additional_info, image
 
+css = """
+h1 {
+    text-align: center;
+    display:block;
+}
+h4 {
+    text-align: center;
+    display:block;
+}
+"""
 
-with gr.Blocks() as demo:
-    with gr.Row():
-        query_box = gr.Textbox(label="Query")
-        upload_box = gr.UploadButton("Upload Images", type="filepath", file_types=["image/*"])
-        dataset_radio = gr.Radio(["minifigures", "bricks"], value="minifigures", label="Model/Dataset")
-        run_button = gr.Button("Run Query")
-    
-    with gr.Row():
-        gallery = gr.Gallery(
-            label="Search results",
-            columns=3,
-            rows=3
-        )
-
+with gr.Blocks(css=css) as interface:
+    with gr.Column():
+        gr.Markdown("# BricksFinder")
+        gr.Markdown("#### *For more information, visit the [GitHub repository](https://github.com/armaggheddon/BricksFinder)*")
+        
     with gr.Row():
         with gr.Column():
-            selected_info = gr.Textbox(label="Additional Information")
-            selected_image_preview = gr.Image(value=None, label="Selected Image", interactive=False)
+            text_query = gr.Textbox(label="Text Query")
+            index_radio = gr.Radio(label="Search for", choices=["minifigure", "brick"], value="minifigure", interactive=True)
+            with gr.Accordion(label="More options", open=False):
+                result_count_slider = gr.Slider(label="Number of Results", minimum=8, maximum=16, value=8, step=1, interactive=True)
+            with gr.Row():
+                clear_btn = gr.Button(value="Clear")
+                search_btn = gr.Button(value="Search", variant="primary")
+        with gr.Column():
+            image_query = gr.Image(label="Image Query")
+    with gr.Row():
+        image_gallery = gr.Gallery(label="Results", columns=4, height="auto", interactive=False)
+    with gr.Row():
+        additional_information = gr.Textbox(label="Additional Information")
+        selected_image = gr.Image(label="Selected Image", interactive=False)
+
     
-    run_button.click(
-        run_query,
-        inputs=[query_box, upload_box, dataset_radio],
-        outputs=[gallery, selected_image_preview]
+    clear_btn.click(
+        lambda: ["", None, None, "", None],
+        outputs=[
+            text_query, 
+            image_query, 
+            image_gallery, 
+            additional_information, 
+            selected_image
+        ],
+        show_progress=False
     )
-    gallery.select(
-        select_image,
-        None, 
-        [selected_info, selected_image_preview]
+    search_btn.click(
+        fn=search,
+        inputs=[
+            text_query,
+            image_query,
+            index_radio,
+            result_count_slider
+        ],
+        outputs=[
+            image_gallery
+        ]
+    )
+    image_gallery.select(
+        get_image_info,
+        inputs=None,
+        outputs=[additional_information, selected_image]
     )
 
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=8000)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--share",
+        action="store_true",
+        help="Share the interface on a public URL"
+    )
+    args = parser.parse_args()
+
+    interface.launch(
+        server_name="0.0.0.0",
+        server_port=8000,
+        share=args.share
+    )
